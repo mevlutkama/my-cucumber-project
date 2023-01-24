0b3ccffa4280285abff81eb6f441326ba00b7d90
Mevlut Kama <mtkama02@gmail.com>
2023-01-24 22:49:27 +0300

    cucumber

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -7,10 +7,17 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {
+                "pretty",
+                "html:target/default-cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"
+        },
+        monochrome=true,
         features = "./src/test/resources/features",
         glue = "stepdefinitions",
         dryRun = false,
-        tags = "@parameterizing1"
+        tags = "@data_tables"
 )
 public class Runner {
 }
@@ -19,4 +26,4 @@ public class Runner {
  glue = "stepdefinitions", //PATH OF STEP DEFINITIONS
  dryRun = false //IF dryRun = true. RUN DRY. JUST GIVE ME THE MISSING STEP DEFINITIONS. DON'T RUN THE TEST CASES.
  dryRun= false when we are doing test execution. dryRun=true when we are generation missing step definitions
-*/
+ */
---
+++ src/test/java/stepdefinitions/DataTablesStepDefinitions.java
@@ -0,0 +1,88 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import pages.DataTablesPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class DataTablesStepDefinitions {
+
+    DataTablesPage dataTablesPage = new DataTablesPage();
+    @Given("user is on {string} page")
+    public void user_is_on_page(String string) {
+        Driver.getDriver().get(string);
+
+    }
+    @Given("user clicks on the new button")
+    public void user_clicks_on_the_new_button() {
+        ReusableMethods.waitFor(1);
+        dataTablesPage.newButton.click();
+
+    }
+    @Given("user enters the first name {string}")
+    public void user_enters_the_first_name(String string) {
+        ReusableMethods.waitFor(1);
+        dataTablesPage.firstName.sendKeys(string);
+
+    }
+    @Given("user enters the last name {string}")
+    public void user_enters_the_last_name(String string) {
+        ReusableMethods.waitFor(1);
+        dataTablesPage.lastName.sendKeys(string);
+
+    }
+    @Given("user enters the position {string}")
+    public void user_enters_the_position(String string) {
+        ReusableMethods.waitFor(1);
+        dataTablesPage.position.sendKeys(string);
+
+    }
+    @Given("user enters office {string}")
+    public void user_enters_office(String string) {
+        ReusableMethods.waitFor(1);
+        dataTablesPage.office.sendKeys(string);
+
+    }
+    @Given("user enters extension {string}")
+    public void user_enters_extension(String string) {
+        ReusableMethods.waitFor(1);
+        dataTablesPage.extension.sendKeys(string);
+
+    }
+    @Given("user enters start date {string}")
+    public void user_enters_start_date(String string) {
+        ReusableMethods.waitFor(1);
+        dataTablesPage.startDate.sendKeys(string);
+
+    }
+    @Given("user enters the salary {string}")
+    public void user_enters_the_salary(String string) {
+        ReusableMethods.waitFor(1);
+        dataTablesPage.salary.sendKeys(string);
+
+    }
+    @Given("click on create button")
+    public void click_on_create_button() {
+        ReusableMethods.waitFor(1);
+        dataTablesPage.createButton.click();
+
+    }
+    @Given("search for the first name {string}")
+    public void search_for_the_first_name(String string) {
+        ReusableMethods.waitFor(1);
+        dataTablesPage.searchBox.sendKeys(string);
+
+    }
+    @Then("verify the name field contains the first name {string}")
+    public void verify_the_name_field_contains_the_first_name(String string) {
+        ReusableMethods.waitFor(1);
+        Assert.assertTrue(dataTablesPage.nameColumn.getText().contains(string));
+    }
+
+
+}
+
