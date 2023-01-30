bc0bbc13fbc122f0dd8d3463fcca77800f2f737e
Mevlut Kama <mtkama02@gmail.com>
2023-01-31 00:28:17 +0300

    cucumber

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -18,7 +18,7 @@ import org.junit.runner.RunWith;
         features = "./src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
         dryRun = false,
-        tags = "@blue_rental"
+        tags = "@failed_tests"
 )
 public class Runner {
 }
