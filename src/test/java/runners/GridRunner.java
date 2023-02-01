f2d802eb7841b6e7484199507293c9b706438492
Mevlut Kama <mtkama02@gmail.com>
2023-02-01 20:02:03 +0300

    cucumber

---
+++ src/test/java/runners/GridRunner.java
@@ -0,0 +1,30 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "html:target/default-cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml",
+                "rerun:target/failedRerun.txt",
+
+        },
+        monochrome=false,
+        features = "./src/test/resources/features",
+        glue = {"stepdefinitions"},
+        dryRun = false,
+        tags = "@grid_firefox"
+)
+public class GridRunner {
+}
+/*
+ features = "./src/test/resources/features",//PATH OF FEATURES FOLDER
+ glue = "stepdefinitions", //PATH OF STEP DEFINITIONS
+ dryRun = false //IF dryRun = true. RUN DRY. JUST GIVE ME THE MISSING STEP DEFINITIONS. DON'T RUN THE TEST CASES.
+ dryRun= false when we are doing test execution. dryRun=true when we are generation missing step definitions
+ */
---
+++ src/test/resources/features/seleniumgrid/selenium_grid_test_case.feature
@@ -0,0 +1,15 @@
+Feature: grid_feature
+
+  @grid_chrome
+  Scenario: TC01_running_tests_on_Chrome
+    Given user is on the application_url using chrome
+    Then verify the page title contains "Techpro Education"
+    Then close application
+
+  @grid_firefox
+  Scenario: TC02_running_tests_on_Firefox
+    Given user is on the application_url using firefox
+    Then verify the page title contains "Techpro Education"
+    Then close application
+
+
