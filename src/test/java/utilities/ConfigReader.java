d1bbfea82d3c043d555ff55de16a3f000efef22e
Mevlut Kama <mtkama02@gmail.com>
2023-02-20 10:42:52 +0300

    cucumber

--- src/test/java/utilities/ConfigReader.java
+++ src/test/java/utilities/ConfigReader.java
@@ -2,8 +2,9 @@ package utilities;
 import java.io.FileInputStream;
 import java.util.Properties;
 public class ConfigReader {
-    //    This class will ONLY be used for getting the data from
-//    configuration.properties file
+
+    // This class will ONLY be used for getting the data from
+    // configuration.properties file
     private static Properties properties;
     static {
 //        path of the config file
