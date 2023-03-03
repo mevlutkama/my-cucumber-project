e1c968eefd138226a0874b1cc5931e269f683323
Mevlut Kama <mtkama02@gmail.com>
2023-03-03 16:40:19 +0300

    cucumber

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -72,6 +72,7 @@ public class ReusableMethods {
         }
         return elemTexts;
     }
+
     //   HARD WAIT WITH THREAD.SLEEP
     //   waitFor(5);  => waits for 5 second
     public static void waitFor(int sec) {
