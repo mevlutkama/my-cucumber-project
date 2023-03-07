80113161fb5465b794fd8f638227541b0d9199dd
Mevlut Kama <mtkama02@gmail.com>
2023-03-07 10:17:38 +0300

    cucumber

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -163,6 +163,7 @@ public class ReusableMethods {
             }
         }
     }
+
     /**
      * Selects a random value from a dropdown list and returns the selected Web Element
      * @param select
