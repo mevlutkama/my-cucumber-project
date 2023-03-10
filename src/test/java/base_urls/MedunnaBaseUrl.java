c0db83121504a43e2b16672906984ee03ea5a7e7
Mevlut Kama <mtkama02@gmail.com>
2023-03-10 12:57:05 +0300

    cucumber

--- src/test/java/base_urls/MedunnaBaseUrl.java
+++ src/test/java/base_urls/MedunnaBaseUrl.java
@@ -4,7 +4,6 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 public class MedunnaBaseUrl {
     public static RequestSpecification spec;
-
     public static void medunnaSetUp(){
         spec = new RequestSpecBuilder().setBaseUri("https://medunna.com").build();
     }
