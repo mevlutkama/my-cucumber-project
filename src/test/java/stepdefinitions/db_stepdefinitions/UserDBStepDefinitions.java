6ef93abaaabbcb9868340afe6734c8befce3e317
Mevlut Kama <mtkama02@gmail.com>
2023-03-13 14:11:11 +0300

    cucumber

--- src/test/java/stepdefinitions/db_stepdefinitions/UserDBStepDefinitions.java
+++ src/test/java/stepdefinitions/db_stepdefinitions/UserDBStepDefinitions.java
@@ -5,19 +5,16 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import utilities.DBUtils;
-
 import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 
 public class UserDBStepDefinitions {
-
     List<Object> columnList;
     @Given("user connects to database")
     public void user_connects_to_database() {
         DBUtils.createConnection();
     }
-
     @When("get all {string} as a list from {string}")
     public void getAllAsAListFrom(String columnName, String tableName) {
         String query = "SELECT * FROM " + tableName;
@@ -32,7 +29,6 @@ public class UserDBStepDefinitions {
     public void closeTheConnection() {
         DBUtils.closeConnection();
     }
-
     @Then("verify list contains {string}")
     public void verifyListContains(String data) {
         assertTrue(columnList.contains(Integer.valueOf(data)));
